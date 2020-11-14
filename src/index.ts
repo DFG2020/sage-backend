bb7e15aa5b11505395950675c1ac200aa1a5a631
shanedharan <shanedharan@gmail.com>
2020-11-14 00:59:33 -0500

    Swapping to NestJS
    - Super sick of all the code I have to write for ExpressJS and using NestJS.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import app from "./app";
-
-// TODO: move to environment variable or configuration.
-const port = 8080;
-
-// start the Express server
-app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-});
