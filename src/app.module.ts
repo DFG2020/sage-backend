2db3373294fef14ca0e7626f466d5e35fd0025c9
shanedharan <shanedharan@gmail.com>
2020-11-14 04:00:27 -0500

    Add type orm to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import {Module} from '@nestjs/common';
 import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {ClientUserModule} from './client-user/client-user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [ClientUserModule],
+  imports: [TypeOrmModule.forRoot(), ClientUserModule],
   controllers: [AppController],
   providers: [AppService],
 })
