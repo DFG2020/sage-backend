b0c2b8d5753ba7bc038c9938778272ef31aa8955
shanedharan <shanedharan@gmail.com>
2020-11-14 00:40:53 -0500

    Add working validation for user

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import { createUser } from "./controller/userController";
 
 const app = express();
+
+app.use(express.json());
 
 // TODO: Add real controllers.
 app.get( "/", ( req, res ) => {
     res.send( "Hello world!" );
 });
+
+app.post("/user", createUser);
 
 export default app;
